46cda53e32577a053a353efb64339b5688b4c931
layla  hammoud <laylahammoud63@gmail.com>
2020-11-14 09:49:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Code your solution in this file!
-let companyName = 'Scuber';
-
-const companyName = "scuber";
+const companyName = "Scuber";
 let mostProfitableNeighborhood = "Chelsea";
-let companyCeo = "han";
+let companyCeo = "Susan Smith";
