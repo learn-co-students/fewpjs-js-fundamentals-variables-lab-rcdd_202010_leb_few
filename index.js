c4071b95be02cca2b5d5ca141fcf96b0d6cd492d
jovial-application-1943 <fatima.katrib@lau.edu>
2020-11-20 20:12:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+// Code your solution in this file
+const companyName = 'Scuber';
+let companyCeo = 'Susan Smith';
+let mostProfitableNeighborhood ='Chelsea';
+describe('index.js', function () {
+  // there's stuff in here, too
+});
+it('is set as Scuber', function () {
+  // tests are here
+});
